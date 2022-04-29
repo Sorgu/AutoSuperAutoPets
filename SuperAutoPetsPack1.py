19ca8add7f7f3f62855834437d8cd80855c1df6f
Sorgu <32864491+Sorgu@users.noreply.github.com>
2022-04-29 11:56:10 +0200

    Create SuperAutoPetsPack1.py

---
+++ SuperAutoPetsPack1.py
@@ -0,0 +1,81 @@
+pets = [
+{
+"name": "cricket",
+"picture": "./pics/SAPpets/cricket.jpg"
+},
+{
+"name": "fish",
+"picture": "./pics/SAPpets/fish.jpg"
+},
+{
+"name": "pig",
+"picture": "./pics/SAPpets/pig.jpg"
+},{
+"name": "beaver",
+"picture": "./pics/SAPpets/beaver.jpg"
+},{
+"name": "mosquito",
+"picture": "./pics/SAPpets/mosquito.jpg"
+},{
+"name": "horse",
+"picture": "./pics/SAPpets/horse.jpg"
+},{
+"name": "otter",
+"picture": "./pics/SAPpets/otter.jpg"
+},{
+"name": "ant",
+"picture": "./pics/SAPpets/ant.jpg"
+},{
+"name": "duck",
+"picture": "./pics/SAPpets/duck.jpg"
+}
+
+]
+
+gold = [
+{
+        "amount": 10,
+        "picture": "./pics/10.jpg"
+    },
+{
+        "amount": 9,
+        "picture": "./pics/9.jpg"
+    },
+{
+        "amount": 8,
+        "picture": "./pics/8.jpg"
+    },
+{
+        "amount": 7,
+        "picture": "./pics/7.jpg"
+    },
+{
+        "amount": 6,
+        "picture": "./pics/6.jpg"
+    },
+{
+        "amount": 5,
+        "picture": "./pics/5.jpg"
+    },
+{
+        "amount": 4,
+        "picture": "./pics/4.jpg"
+    },
+{
+        "amount": 3,
+        "picture": "./pics/3.jpg"
+    },
+{
+        "amount": 2,
+        "picture": "./pics/2.jpg"
+    },
+{
+        "amount": 1,
+        "picture": "./pics/1.jpg"
+    },
+{
+        "amount": 0,
+        "picture": "./pics/0.jpg"
+    },
+
+]
